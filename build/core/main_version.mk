94c7fe02b6fc3e10e64ad06140f127dfd399fb00
predator <mi460334@gmail.com>
2021-01-28 13:40:03 +0000

    predator

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,3 +1,8 @@
+# Set all versions
+PREDATOR_BUILD_TYPE ?= UNOFFICIAL
+PREDATOR_VERSION_BASE := 1.0
+PREDATOR_CODENAME := Rembang-Jateng
+
 # Build fingerprint
 ifneq ($(BUILD_FINGERPRINT),)
 ADDITIONAL_BUILD_PROPERTIES += \
@@ -7,6 +12,9 @@ endif
 # Predator System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
+    ro.predator.codename=$(PREDATOR_CODENAME) \
+    ro.lineage.build.type=$(PREDATOR_BUILD_TYPE) \
+    ro.lineage.build.version=$(PREDATOR_VERSION_BASE) \
     ro.lineage.display.version=$(LINEAGE_DISPLAY_VERSION) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(LINEAGE_VERSION)
